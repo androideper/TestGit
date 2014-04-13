365b8f0dfb7038de001e106c5a81df88f33372cc
androideper <androideper@gmail.com>
2014-04-13 22:45:49 +0800

    Local commit

--- src/main/test/com/luckyryan/sample/myexcrcises/StackExerciseTest.java
+++ src/main/test/com/luckyryan/sample/myexcrcises/StackExerciseTest.java
@@ -116,14 +116,18 @@ public class StackExerciseTest extends TestCase {
 
             assertTrue("After popping element from stack with one element it must become empty",
                     stackExercise.isEmpty());
-            assertEquals("Top element and popped element of stack must be the same",topElement, poppedElement);
+            assertEquals("Top element and popped element of stack must be the same", topElement, poppedElement);
 
         } catch (StackEmptyException e) {
             fail("Stack is not empty. This exception is invalid");
         }
     }
 
-    public void testGit(){
+    public void testGit() {
+
+    }
+
+    public void testThirdFork() {
 
     }
 
